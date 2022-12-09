817ade5a4be61ebc259f787c4c55de357b988621
faizwahid <faizwahidadda@gmail.com>
2022-12-10 01:36:27 +0800

    first commit

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+//DB Params
+define('DB_HOST','localhost');
+define('DB_USER','_Your_User_');
+define('DB_PASS','_Your_Pass_');
+define('DB_NAME','_Your_DBNAME');
+
+// app root
+define('APPROOT', dirname(dirname(__FILE__)));
+// url root
+define('URLROOT','_YourLocalhostURL_');
+//Site name
+define('SITENAME', '_YourSiteName_');
---
+++ app/views/pages/index.php
@@ -0,0 +1,7 @@
+<?php require APPROOT. '/views/inc/header.php'; ?>
+<h1><?php echo $data['title']; ?></h1>
+<p>This is the FaizMVC PHP framework.</p>
+<p> You need to edit on app/congif/config.php 
+    and on public/.htaccess line 4
+</p>
+<?php require APPROOT. '/views/inc/footer.php'; ?>
